e3aeff458ddd3c33adde8980151a198118fa47d7
kiaraa-ccala <kiaraa.ccala@pucp.edu.pe>
2025-04-11 14:09:43 -0500

    Creacion del proyecto Spring Boot

---
+++ ProyectoGestionServiciosDeportivos/GSP/src/test/java/tcps/gsp/GspApplicationTests.java
@@ -0,0 +1,13 @@
+package tcps.gsp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GspApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
