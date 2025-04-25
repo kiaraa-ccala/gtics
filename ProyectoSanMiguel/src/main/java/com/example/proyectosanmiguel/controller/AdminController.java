5c80ecefb1e6ab20ab428f92e9c47b52e9293880
kiaraa-ccala <kiaraa.ccala@pucp.edu.pe>
2025-04-25 16:19:06 -0500

    es trabajo honesto- superadmin
    espero no chocar con tony

---
+++ ProyectoSanMiguel/src/main/java/com/example/proyectosanmiguel/controller/AdminController.java
@@ -0,0 +1,17 @@
+package com.example.proyectosanmiguel.controller;
+import com.example.proyectosanmiguel.entity.ComplejoDeportivo;
+import com.example.proyectosanmiguel.entity.Usuario;
+import com.example.proyectosanmiguel.repository.UsuarioRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+import java.util.Optional;
+
+public class AdminController {
+
+
+}
