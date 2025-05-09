373fae3c68785df1931c481cadb1c5c6e2755d46
Tony F. <a20223209@pucp.edu.pe>
2025-05-09 18:16:51 -0500

    Se agrega la dependencia

--- ProyectoSanMiguel/src/main/java/com/example/proyectosanmiguel/controller/VecinoController2.java
+++ ProyectoSanMiguel/src/main/java/com/example/proyectosanmiguel/controller/VecinoController2.java
@@ -1,10 +1,16 @@
 package com.example.proyectosanmiguel.controller;
+import com.example.proyectosanmiguel.entity.InstanciaServicio;
+import com.example.proyectosanmiguel.entity.Reserva;
+import com.example.proyectosanmiguel.entity.Servicio;
 import com.example.proyectosanmiguel.repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
 
 @Controller
 @RequestMapping("/vecino")
@@ -32,7 +38,15 @@ public class VecinoController2 {
     private ReservaRepository reservaRepository;
 
     @GetMapping("/crearReserva")
-    public String crearReserva(Model model) {
+    public String crearReserva(Model model, @ModelAttribute("reserva") Reserva reserva) {
+
+        List<Servicio> servicios = servicioRepository.findAll();
+
+        return "Vecino/vecino_formulario_complejo";
+    }
+
+    @GetMapping("/guardarReserva")
+    public String guardarReserva(@ModelAttribute Reserva reserva) {
 
         return "Vecino/vecino_formulario_complejo";
     }
--- ProyectoSanMiguel/src/main/java/com/example/proyectosanmiguel/repository/InstanciaServicioRepository.java
+++ ProyectoSanMiguel/src/main/java/com/example/proyectosanmiguel/repository/InstanciaServicioRepository.java
@@ -1,9 +1,15 @@
 package com.example.proyectosanmiguel.repository;
 
 import com.example.proyectosanmiguel.entity.InstanciaServicio;
+import com.example.proyectosanmiguel.entity.Servicio;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface InstanciaServicioRepository extends JpaRepository<InstanciaServicio, Integer> {
+
+    List<Servicio> findInstanciaServicioByComplejoDeportivo_IdComplejoDeportivo(Integer id);
+
 }
