6362d5688355feccb1d25b6b87a0755a0cf01e6b
AlpacaTaka <109617843+AlpacaTaka@users.noreply.github.com>
2025-05-20 22:06:50 -0500

    servicio de email listo :D

---
+++ ProyectoSanMiguel/src/main/java/com/example/proyectosanmiguel/service/EmailService.java
@@ -0,0 +1,46 @@
+package com.example.proyectosanmiguel.service;
+
+import com.sendgrid.Method;
+import com.sendgrid.Request;
+import com.sendgrid.Response;
+import com.sendgrid.SendGrid;
+import com.sendgrid.helpers.mail.Mail;
+import com.sendgrid.helpers.mail.objects.Content;
+import com.sendgrid.helpers.mail.objects.Email;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+
+@Service
+public class EmailService {
+
+    @Value("${spring.sendgrid.api-key}")
+    private String sendGridApiKey;
+
+    public void enviarEmail(String destinatario, String asunto, String contenidoTexto) throws IOException {
+        Email from = new Email("no-replay@deportesanmiguelpucp.online"); // remitente autorizado
+        Email to = new Email(destinatario);
+        Content content = new Content("text/plain", contenidoTexto);
+        Mail mail = new Mail(from, asunto, to, content);
+
+        SendGrid sg = new SendGrid(sendGridApiKey);
+        Request request = new Request();
+
+        try {
+            request.setMethod(Method.POST);
+            request.setEndpoint("mail/send");
+            request.setBody(mail.build());
+            Response response = sg.api(request);
+
+            System.out.println("Status code: " + response.getStatusCode());
+            System.out.println("Body: " + response.getBody());
+            System.out.println("Headers: " + response.getHeaders());
+
+        } catch (IOException ex) {
+            throw ex;
+        }
+    }
+
+}
