d1bddb455ab6d707c1241e9359bfd43133c0a037
kiaraa-ccala <kiaraa.ccala@pucp.edu.pe>
2025-06-13 18:47:00 -0500

    landing

--- ProyectoSanMiguel/src/main/java/com/example/proyectosanmiguel/config/AppConfig.java
+++ ProyectoSanMiguel/src/main/java/com/example/proyectosanmiguel/config/AppConfig.java
@@ -6,15 +6,9 @@ import org.apache.http.impl.client.HttpClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
-import org.springframework.web.reactive.function.client.WebClient;
 
 @Configuration
 public class AppConfig {
-
-    @Bean
-    public WebClient.Builder webClientBuilder() {
-        return WebClient.builder();
-    }
 
     @Bean
     public HttpClient httpClient() {
@@ -23,6 +17,7 @@ public class AppConfig {
                 .setMaxConnPerRoute(20) // Configura el número máximo de conexiones por ruta
                 .build();
     }
+    
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
